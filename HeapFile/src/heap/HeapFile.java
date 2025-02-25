f08b03ed6f286fedbc7027c06a5e6f60f62d35dd
dch777 <d.chanana777@gmail.com>
2025-02-25 17:59:28 -0500

    Parts 2 and 3 mostly completed

--- HeapFile/src/heap/HeapFile.java
+++ HeapFile/src/heap/HeapFile.java
@@ -1,7 +1,9 @@
 package heap;
 
-import global.GlobalConst;
-import global.RID;
+import java.io.*;
+import java.util.*;
+import diskmgr.*;
+import global.*;
 
 /**
  * <h3>Minibase Heap Files</h3>
@@ -13,13 +15,105 @@ import global.RID;
  */
 public class HeapFile implements GlobalConst {
 
+  private String name;
+  private PageId head;
+  private PageId tail;
+
+  private TreeMap<Short, HashSet<PageId>> freepages;
+
   /**
    * If the given name already denotes a file, this opens it; otherwise, this
    * creates a new empty file. A null name produces a temporary heap file which
    * requires no DB entry.
    */
   public HeapFile(String name) {
-      //PUT YOUR CODE HERE
+    this.name = name;
+    freepages = new TreeMap<Short, HashSet<PageId>>();
+
+    head = Minibase.DiskManager.get_file_entry(name);
+    HFPage apage = new HFPage();
+    if (head != null) {
+      tail = head;
+      Minibase.BufferManager.pinPage(tail, apage, false);
+      while (apage.getNextPage().pid >= 0) {
+        addFreePage(tail);
+        PageId tmp = apage.getNextPage();
+        Minibase.BufferManager.unpinPage(tail, false);
+        tail = tmp;
+        Minibase.BufferManager.pinPage(tail, apage, false);
+      }
+      Minibase.BufferManager.unpinPage(tail, false);
+    } else {
+      head = Minibase.BufferManager.newPage(apage, 1);
+      tail = head;
+      apage.setCurPage(head);
+
+      if (name != null) {
+        Minibase.DiskManager.add_file_entry(name, head);
+      }
+
+      addFreePage(head);
+      Minibase.BufferManager.unpinPage(head, true);
+    }
+  }
+
+  protected void addFreePage(PageId p) {
+    HFPage apage = new HFPage();
+    Minibase.BufferManager.pinPage(p, apage, false);
+    if (apage.getFreeSpace() == 0) {
+      Minibase.BufferManager.unpinPage(p, false);
+      return;
+    }
+
+    HashSet<PageId> set = freepages.get(apage.getFreeSpace());
+    if (set == null) {
+      set = new HashSet<PageId>();
+      freepages.put(apage.getFreeSpace(), set);
+    }
+    set.add(p);
+    Minibase.BufferManager.unpinPage(p, false);
+  }
+
+  protected boolean removeFreePage(PageId p) {
+    HFPage apage = new HFPage();
+    Minibase.BufferManager.pinPage(p, apage, false);
+    short size = apage.getFreeSpace();
+    Minibase.BufferManager.unpinPage(p, false);
+
+    HashSet<PageId> set = freepages.get(size);
+    if (set == null || !set.remove(p)) {
+      return false;
+    }
+
+    if (set.size() == 0) {
+      freepages.remove(size);
+    }
+    return true;
+  }
+
+  protected PageId getFreePage(short size) throws SpaceNotAvailableException {
+    if (freepages.higherEntry(size) == null ||
+        freepages.higherEntry(size).getValue().size() == 0 ||
+        freepages.higherEntry(size).getKey() - size <= 0) {
+      HFPage apage = new HFPage();
+      PageId p = Minibase.BufferManager.newPage(apage, 1);
+      if (apage.getFreeSpace() < size) {
+        Minibase.BufferManager.unpinPage(p, true);
+        Minibase.BufferManager.freePage(p);
+        throw new SpaceNotAvailableException("requested size is bigger than a page");
+      }
+
+      apage.setPrevPage(tail);
+      apage.setCurPage(p);
+      Minibase.BufferManager.pinPage(tail, apage, false);
+      apage.setNextPage(p);
+      Minibase.BufferManager.unpinPage(tail, true);
+      tail = p;
+
+      addFreePage(p);
+      Minibase.BufferManager.unpinPage(p, true);
+    }
+    return freepages.higherEntry(size).getValue().stream().findFirst().get();
   }
 
   /**
@@ -27,14 +121,27 @@ public class HeapFile implements GlobalConst {
    * object; deletes the heap file if it's temporary.
    */
   protected void finalize() throws Throwable {
-      //PUT YOUR CODE HERE
+    if (name.equals(null)) {
+      deleteFile();
+    }
   }
 
   /**
    * Deletes the heap file from the database, freeing all of its pages.
    */
   public void deleteFile() {
-    //PUT YOUR CODE HERE
+    PageId p = head;
+    HFPage apage = new HFPage();
+
+    Minibase.BufferManager.pinPage(p, apage, false);
+    while (apage.getNextPage().pid >= 0) {
+      PageId tmp = apage.getNextPage();
+      Minibase.BufferManager.unpinPage(p, false);
+      Minibase.BufferManager.freePage(p);
+      p = tmp;
+      Minibase.BufferManager.pinPage(p, apage, false);
+    }
+    Minibase.BufferManager.unpinPage(p, false);
   }
 
   /**
@@ -42,8 +149,17 @@ public class HeapFile implements GlobalConst {
    * 
    * @throws IllegalArgumentException if the record is too large
    */
-  public RID insertRecord(byte[] record) {
-    //PUT YOUR CODE HERE
+  public RID insertRecord(byte[] record) throws SpaceNotAvailableException {
+    PageId p = getFreePage((short) (record.length + 1));
+    HFPage apage = new HFPage();
+
+    Minibase.BufferManager.pinPage(p, apage, false);
+    removeFreePage(p);
+    RID rid = apage.insertRecord(record);
+    addFreePage(p);
+    Minibase.BufferManager.unpinPage(p, true);
+
+    return rid;
   }
 
   /**
@@ -51,8 +167,29 @@ public class HeapFile implements GlobalConst {
    * 
    * @throws IllegalArgumentException if the rid is invalid
    */
-  public byte[] selectRecord(RID rid) {
-    //PUT YOUR CODE HERE
+  public Tuple getRecord(RID rid) {
+    PageId p = head;
+    HFPage apage = new HFPage();
+
+    Minibase.BufferManager.pinPage(p, apage, false);
+    while (apage.getNextPage().pid >= 0) {
+      if (apage.getCurPage().equals(rid.pageno)) {
+        byte[] record = apage.selectRecord(rid);
+        return new Tuple(record, 0, record.length);
+      }
+
+      PageId tmp = apage.getNextPage();
+      Minibase.BufferManager.unpinPage(p, false);
+      p = tmp;
+      Minibase.BufferManager.pinPage(p, apage, false);
+    }
+    if (apage.getCurPage().equals(rid.pageno)) {
+      byte[] record = apage.selectRecord(rid);
+      return new Tuple(record, 0, record.length);
+    }
+    Minibase.BufferManager.unpinPage(p, false);
+
+    throw new IllegalArgumentException("RID pageno not in HeapFile");
   }
 
   /**
@@ -60,8 +197,14 @@ public class HeapFile implements GlobalConst {
    * 
    * @throws IllegalArgumentException if the rid or new record is invalid
    */
-  public void updateRecord(RID rid, byte[] newRecord) {
-    //PUT YOUR CODE HERE
+  public boolean updateRecord(RID rid, Tuple newRecord) throws InvalidUpdateException {
+    HFPage apage = new HFPage();
+
+    Minibase.BufferManager.pinPage(rid.pageno, apage, false);
+    apage.updateRecord(rid, newRecord);
+    Minibase.BufferManager.unpinPage(rid.pageno, true);
+
+    return true;
   }
 
   /**
@@ -69,15 +212,36 @@ public class HeapFile implements GlobalConst {
    * 
    * @throws IllegalArgumentException if the rid is invalid
    */
-  public void deleteRecord(RID rid) {
-    //PUT YOUR CODE HERE
+  public boolean deleteRecord(RID rid) {
+    HFPage apage = new HFPage();
+    Minibase.BufferManager.pinPage(rid.pageno, apage, false);
+    apage.deleteRecord(rid);
+    Minibase.BufferManager.unpinPage(rid.pageno, true);
+
+    return true;
   }
 
   /**
    * Gets the number of records in the file.
    */
   public int getRecCnt() {
-    //PUT YOUR CODE HERE
+    int recCnt = 0;
+
+    PageId p = head;
+    HFPage apage = new HFPage();
+
+    Minibase.BufferManager.pinPage(p, apage, false);
+    while (apage.getNextPage().pid >= 0) {
+      recCnt += apage.getSlotCount();
+      PageId tmp = apage.getNextPage();
+      Minibase.BufferManager.unpinPage(p, false);
+      p = tmp;
+      Minibase.BufferManager.pinPage(p, apage, false);
+    }
+    recCnt += apage.getSlotCount();
+    Minibase.BufferManager.unpinPage(p, false);
+
+    return recCnt;
   }
 
   /**
@@ -91,7 +255,10 @@ public class HeapFile implements GlobalConst {
    * Returns the name of the heap file.
    */
   public String toString() {
-    //PUT YOUR CODE HERE
+    return name;
   }
 
+  public PageId getHead() {
+    return head;
+  }
 } // public class HeapFile implements GlobalConst
--- HeapFile/src/heap/HeapScan.java
+++ HeapFile/src/heap/HeapScan.java
@@ -1,8 +1,10 @@
 package heap;
 
 import java.util.*;
-import global.* ;
+import global.*;
 import chainexception.ChainException;
+import diskmgr.*;
+import global.*;
 
 /**
  * A HeapScan object is created only through the function openScan() in the
@@ -11,13 +13,19 @@ import chainexception.ChainException;
  */
 public class HeapScan implements GlobalConst {
 
+  private HFPage currentPage;
+  private RID currentRid;
+  private boolean open;
+
   /**
    * Constructs a file scan by pinning the directoy header page and initializing
    * iterator fields.
    */
   protected HeapScan(HeapFile hf) {
-    //PUT YOUR CODE HERE
-    
+    currentPage = new HFPage();
+    Minibase.BufferManager.pinPage(hf.getHead(), currentPage, false);
+    currentRid = currentPage.firstRecord();
+    open = true;
   }
 
   /**
@@ -25,21 +33,24 @@ public class HeapScan implements GlobalConst {
    * object; closes the scan if it's still open.
    */
   protected void finalize() throws Throwable {
-    //PUT YOUR CODE HERE
+    close();
   }
 
   /**
    * Closes the file scan, releasing any pinned pages.
    */
-  public void close() {
-    //PUT YOUR CODE HERE
+  public void close() throws InvalidUpdateException {
+    if (open) {
+      Minibase.BufferManager.unpinPage(currentPage.getCurPage(), true);
+      open = false;
+    }
   }
 
   /**
    * Returns true if there are more records to scan, false otherwise.
    */
   public boolean hasNext() {
-    //PUT YOUR CODE HERE
+    return currentRid == null;
   }
 
   /**
@@ -48,8 +59,32 @@ public class HeapScan implements GlobalConst {
    * @param rid output parameter that identifies the returned record
    * @throws IllegalStateException if the scan has no more elements
    */
-  public byte[] getNext(RID rid) {
-    //PUT YOUR CODE HERE
+  public Tuple getNext(RID rid) {
+    if (currentRid == null) {
+      Minibase.BufferManager.unpinPage(currentPage.getCurPage(), true);
+      open = false;
+      return null;
+    }
+
+    rid.copyRID(currentRid);
+    byte[] record = currentPage.selectRecord(rid);
+    Tuple currentRecord = new Tuple(record, 0, record.length);
+
+    currentRid = currentPage.nextRecord(currentRid);
+    if (currentRid == null) {
+      PageId currentPageId = currentPage.getCurPage();
+      PageId nextPageId = currentPage.getNextPage();
+
+      if (nextPageId.pid >= 0) {
+        Minibase.BufferManager.unpinPage(currentPageId, true);
+        Minibase.BufferManager.pinPage(nextPageId, currentPage, false);
+        currentRid = currentPage.firstRecord();
+      } else {
+        currentRid = null;
+      }
+    }
+
+    return currentRecord;
   }
 
 } // public class HeapScan implements GlobalConst
